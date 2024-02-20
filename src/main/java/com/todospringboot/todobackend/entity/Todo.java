02c05beeb8e5b4c730fecd897be2a5d0ce8cce70
Sumit Mansukhlal Savaliya <sm572004@dal.ca>
2024-02-20 12:08:54 -0400

    Chore: Todo Entity

---
+++ src/main/java/com/todospringboot/todobackend/entity/Todo.java
@@ -0,0 +1,28 @@
+package com.todospringboot.todobackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed;
+
+}
