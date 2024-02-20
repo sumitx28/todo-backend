ecc79513d471844a75bb1c93c18e889c1b0ed9de
Sumit Mansukhlal Savaliya <sm572004@dal.ca>
2024-02-20 12:17:15 -0400

    feat: Todo repository and dto setup

---
+++ src/main/java/com/todospringboot/todobackend/dto/TodoDto.java
@@ -0,0 +1,21 @@
+package com.todospringboot.todobackend.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoDto {
+    private Long id;
+    @NotEmpty(message = "title must be provided")
+    private String title;
+    @NotEmpty(message = "description must be provided")
+    private String description;
+    @NotEmpty(message = "completed flag must not be empty")
+    private boolean completed;
+}
